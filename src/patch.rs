c0ad548a22375c810f027a612aede4e4ed1cf415
Waffle <waffle.lapkin@gmail.com>
2020-11-11 22:06:20 +0300

    fixes & workarounds

--- src/patch.rs
+++ src/patch.rs
@@ -127,25 +127,43 @@ impl crate::schema::Doc {
 }
 
 fn intra_links(doc: &mut crate::schema::Doc) {
-    let mut repls = Vec::new();
+    let mut repls_t = Vec::new();
+    let mut repls_m = Vec::new();
+
     doc.md_links
         .iter_mut()
         .filter(|(k, v)| {
             v.starts_with("https://core.telegram.org/bots/api#")
-                && !k.contains(&['-', '_', '.'][..])
+                && !k.contains(&['-', '_', '.', ' '][..])
         })
         .for_each(|(k, v)| {
             if let Some(c) = k.chars().next() {
-                repls.push(k.clone());
                 kiam::when! {
-                    c.is_lowercase() => *v = format!("crate::payloads::{}", k),
-                    c.is_uppercase() => *v = format!("crate::types::{}", k),
+                    k == "unbanned" => *v = String::from("crate::payloads::UnbanChatMember"),
+                    c.is_lowercase() && !["update", "games", "videos", "photos"].contains(&&&**k) => {
+                        repls_m.push(k.clone());
+                        *v = format!("crate::payloads::{}", to_uppercase(k));
+                    },
+                    _ => {
+                        repls_t.push(k.clone());
+                        *v = format!("crate::types::{}", k);
+                    },
                 }
             }
         });
 
-    for repl in repls {
+    for repl in repls_t {
         if let Some(value) = doc.md_links.remove(repl.as_str()) {
+            doc.md = doc
+                .md
+                .replace(format!("[{}]", repl).as_str(), &format!("[`{}`]", repl));
+            doc.md_links.insert(format!("`{}`", repl), value);
+        }
+    }
+
+    for repl in repls_m {
+        if let Some(value) = doc.md_links.remove(repl.as_str()) {
+            let repl = to_uppercase(&repl);
             doc.md = doc
                 .md
                 .replace(format!("[{}]", repl).as_str(), &format!("[`{}`]", repl));
@@ -158,4 +176,9 @@ fn escape_kw(s: &mut String) {
     if ["type"].contains(&s.as_str()) {
         *s = format!("r#{}", s);
     }
+}
+
+fn to_uppercase(s: &str) -> String {
+    let mut chars = s.chars();
+    dbg!(format!("{}{}", chars.next().unwrap().to_uppercase(), chars.as_str()))
 }
